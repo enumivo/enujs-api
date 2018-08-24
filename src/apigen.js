5cbee9c4a5bdae6e9065036342e39e31a352c03b
enumivo <admin@enumivo.org>
2018-08-24 22:22:50 +0800

    use api.enumivo.org

--- src/apigen.js
+++ src/apigen.js
@@ -7,7 +7,7 @@ module.exports = apiGen
 
 function apiGen (version, definitions, config = {}) {
   const configDefaults = {
-    httpEndpoint: 'https://api.enumivo.com',
+    httpEndpoint: 'https://api.enumivo.org',
     verbose: false,
     logger: {
       log: (...args) => config.verbose ? console.log(...args) : null,
